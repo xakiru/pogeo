24e638478383e1d1ce951ca5a1f84abd80dda15c
David Christenson <david@the-david.com>
2017-02-14 22:34:25 -0700

    Revert "Try LinuxBrew"
    This reverts commit c4cb14732c6e3a1a196b53031fee385c67df5114.

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-set -x -e
+set -x
 
+yum install -y python-hashlib
+curl https://copr.fedorainfracloud.org/coprs/mlampe/llvm_381_el5/repo/epel-5/mlampe-llvm_381_el5-epel-5.repo -o /etc/yum.repos.d/mlampe-llvm_381_el5-epel-5.repo
 yum update -y
-yum -y groupinstall 'Development Tools'
-yum install -y curl git irb python-setuptools ruby
+yum install -y openssl-devel
+yum install -y clang
+#yum install -y devtoolset-2 devtoolset-2-gcc-c++-4.8.2 devtoolset-2-libstdc++-devel
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-export PATH="$HOME/.linuxbrew/bin:$PATH"
+set -e
 
-brew install gcc@5
-export CC=g++-5
-export CXX=g++-5
+#source /opt/rh/devtoolset-2/enable
+export CC=clang++
+export CXX=clang++
+#ln -sf /usr/bin/clang-3.8 /usr/bin/gcc
 
 # Compile wheels
 for PYBIN in /opt/python/cp3*/bin; do
