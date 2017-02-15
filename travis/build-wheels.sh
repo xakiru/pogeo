06cd70c8666332e72e82c4c18ebdc5b333f7828c
David Christenson <david@the-david.com>
2017-02-14 17:38:24 -0700

    fix

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -e -x
 
-yum install python-hashlib
+yum install -y python-hashlib
 curl https://copr.fedorainfracloud.org/coprs/mlampe/llvm_381_el5/repo/epel-5/mlampe-llvm_381_el5-epel-5.repo -o /etc/yum.repos.d/mlampe-llvm_381_el5-epel-5.repo
-sed -i 's|https://|http://|g' /etc/yum.repos.d/mlampe-llvm_381_el5-epel-5.repo
 yum update -y
-yum install -y yum-plugin-copr
 yum install -y openssl-devel
-yum update -y
-yum install clang
+yum install -y clang
 
 # Compile wheels
 for PYBIN in /opt/python/cp3*/bin; do
