90ab05447f0a998b23a44ded259a73c8f6c4c1d2
David Christenson <david@the-david.com>
2017-02-16 02:38:20 -0700

    Fix bottling

--- travis/build-bottle.sh
+++ travis/build-bottle.sh
@@ -6,6 +6,6 @@ cd travis
 brew uninstall python3
 xcode-select --install || TRUE
 brew install --build-bottle python@3.5.rb
-brew bottle python@3.5
+brew bottle --force-core-tap python@3.5
 curl --upload-file python*bottle* https://transfer.sh/
 cd ..
