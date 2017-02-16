9601d2ee85a8f36295467b3bdb6b8cd65b9a7437
David Christenson <david@the-david.com>
2017-02-16 02:05:16 -0700

    Fix Python 3.5 formula

--- travis/build-bottle.sh
+++ travis/build-bottle.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+
 cd travis
 brew uninstall python3
+xcode-select --install || TRUE
 brew install --build-bottle python@3.5.rb
 brew bottle python@3.5
 curl --upload-file python*bottle* https://transfer.sh/
