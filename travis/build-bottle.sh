bedbf01907e62a947098747f2b070b81fcdbba36
David Christenson <david@the-david.com>
2017-02-16 21:19:44 -0700

    Simplify

--- travis/build-bottle.sh
+++ travis/build-bottle.sh
@@ -4,7 +4,6 @@ set -e
 
 cd travis
 brew uninstall python3
-xcode-select --install || TRUE
 brew install --build-bottle python35.rb
 brew bottle --force-core-tap python35
 curl --upload-file python*bottle* https://transfer.sh/
